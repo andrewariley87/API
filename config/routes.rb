11cd061c536746ee9766177ba88dbf0911cb572e
andrewariley87 <andrewariley87@gmail.com>
2015-08-27 15:30:35 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,13 @@ Rails.application.routes.draw do
 
     resources :tags, except: [:new, :edit]
 
-    get 'notes/tag/:name' => 'tags#show'
+    get 'notes/tag/:name' => 'api/xstags#show'
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'notes#index'
+  root 'api/notes#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
